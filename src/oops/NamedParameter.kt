a3a19d0ed6c16a1dcd17538fdfd1f7c12477718b
LuisMhaske <luis.mhaske007@gmail.com>
2021-02-08 18:01:45 +0000

    updated

--- src/oops/NamedParameter.kt
+++ src/oops/NamedParameter.kt
@@ -1,9 +1,12 @@
 package oops
 
 
-class House(val height: Double, val color: String,val price: Int) {
+class House(val height: Double, val color: String,val price: Int = 5000000) {
     fun print(){
         println("House details [Height: $height, Color: $color, Price: $price]")
+    }
+    fun sum(i: Int, j: Int){
+        println(i+j)
     }
 
 }
@@ -11,7 +14,11 @@ class House(val height: Double, val color: String,val price: Int) {
 fun main() {
     val house = House(color = "Yellow", height = 7.0, price = 1500000) //named parameters are useful as it helps you define the parameter in any order without affecting the working of the code
     val house2 = House(7.6,"Light Blue", 20000000)
+    val house3 = House(color = "yellow",height = 6.8)
 
     house.print()
+    house.sum(1,2)
     house2.print()
+    house3.print()
+
 }
